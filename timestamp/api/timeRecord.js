b24dee3c9235e599092cd5cf5aa3ffda82a6127b
ttaaarrr <patanasaktar@gmail.com>
2025-09-10 14:50:50 +0700

    timeRecord

--- timestamp/api/timeRecord.js
+++ timestamp/api/timeRecord.js
@@ -1,11 +1,11 @@
 import axios from 'axios';
 
 export async function logTime({ empId, type }) {
-  const res = await axios.post('/api/time-record', { empId, type });
+  const res = await axios.post('https://api-checkin-out.bpit-staff.com/api/time-record', { empId, type });
   return res.data;
 }
 
 export async function getTimeRecords(empId) {
-  const res = await axios.get(`/api/time-record/${empId}`);
+  const res = await axios.get(`https://api-checkin-out.bpit-staff.com/api/time-record/${empId}`);
   return res.data;
 }
