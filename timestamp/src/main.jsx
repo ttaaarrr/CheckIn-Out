306390f2b271fd95965328f6df661393a266b7b7
ttaaarrr <patanasaktar@gmail.com>
2025-09-11 13:10:00 +0700

    main

--- timestamp/src/main.jsx
+++ timestamp/src/main.jsx
@@ -2,22 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
-import Employees from '../pages/Employees.jsx';
-
-const path= createBrowserRouter([
-  [
-  {
-    path: '/login',
-    element: <Employees />,
-  },
-]
-]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={path}>
     <App />
-    </RouterProvider>
   </StrictMode>,
 )
