ac5bc2491b6f03d96c2e7d396f0491b40009b08b
ttaaarrr <patanasaktar@gmail.com>
2025-09-11 10:56:43 +0700

    Employees

--- timestamp/pages/Employees.jsx
+++ timestamp/pages/Employees.jsx
@@ -4,7 +4,19 @@ import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import { MapContainer, TileLayer, Marker, useMap, useMapEvents } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
+import L from 'leaflet';
+import markerIcon2x from 'leaflet/dist/images/marker-icon-2x.png';
+import markerIcon from 'leaflet/dist/images/marker-icon.png';
+import markerShadow from 'leaflet/dist/images/marker-shadow.png';
 
+// Fix default icon
+delete L.Icon.Default.prototype._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: markerIcon2x,
+  iconUrl: markerIcon,
+  shadowUrl: markerShadow,
+});
 const MarkerSetter = ({ lat, lng, setLat, setLng }) => {
   const map = useMap();
 
