ba7fdb4bdf44f4ba49675b1c67fe9f574173145e
ttaaarrr <patanasaktar@gmail.com>
2025-09-11 13:06:05 +0700

    main

--- timestamp/src/main.jsx
+++ timestamp/src/main.jsx
@@ -3,12 +3,13 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import Employees from '../pages/Employees.jsx';
 
 const path= createBrowserRouter([
   [
   {
     path: '/login',
-    element: <Checkin />,
+    element: <Employees />,
   },
 ]
 ]);
