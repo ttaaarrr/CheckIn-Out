d4d79804084e0ed6d0cbc140aa592f9d918f53aa
ttaaarrr <93063822+ttaaarrr@users.noreply.github.com>
2025-12-04 16:51:44 +0700

    Refactor Navbar to hide on root path

--- timestamp/components/Navbaar.jsx
+++ timestamp/components/Navbaar.jsx
@@ -1,6 +1,10 @@
-import { NavLink } from 'react-router-dom';
+import { NavLink,useLocation } from 'react-router-dom';
 
 export default function Navbar() {
+  const location = useLocation();
+
+  // ❌ ถ้าอยู่หน้าลงเวลา "/" ให้ไม่แสดง Navbar
+  if (location.pathname === "/") return null;
   const activeClass = "bg-blue-500 text-white px-6 py-3 rounded-lg mx-1";
   const normalClass = "bg-gray-100 text-gray-700 px-6 py-3 rounded-lg mx-1";
 
@@ -13,36 +17,3 @@ export default function Navbar() {
     </div>
   );
 }
-
-// เผื่อใช้
-// import { NavLink } from 'react-router-dom';
-// import { useState, useEffect } from 'react';
-
-// export default function Navbar() {
-//   const activeClass = "bg-blue-500 text-white px-6 py-3 rounded-lg mx-1";
-//   const normalClass = "bg-gray-100 text-gray-700 px-6 py-3 rounded-lg mx-1";
-
-//   // สมมติว่า role มาจาก backend หรือ localStorage
-//   const [role, setRole] = useState('user'); // default = user
-
-//   useEffect(() => {
-//     // ในอนาคต fetch role ของผู้ใช้งานจาก backend
-//     // ตัวอย่างดึงจาก localStorage
-//     const userRole = localStorage.getItem('role') || 'user';
-//     setRole(userRole);
-//   }, []);
-
-//   return (
-//     <div className="flex justify-center mb-8">
-   
-      
-//       {role === 'admin' && (
-//         <>
-//           <NavLink to="/" className={({ isActive }) => isActive ? activeClass : normalClass}>ลงเวลา</NavLink>
-//           <NavLink to="/employees" className={({ isActive }) => isActive ? activeClass : normalClass}>จัดการพนักงาน</NavLink>
-//           <NavLink to="/dashboard" className={({ isActive }) => isActive ? activeClass : normalClass}>แดชบอร์ด</NavLink>
-//         </>
-//       )}
-//     </div>
-//   );
-// }
