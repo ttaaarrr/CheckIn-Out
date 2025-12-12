d8470de540f8e38b77acc39c5fd0f946f85e84a8
ttaaarrr <93063822+ttaaarrr@users.noreply.github.com>
2025-12-12 17:20:24 +0700

    Update Dashboard.jsx

--- timestamp/pages/Dashboard.jsx
+++ timestamp/pages/Dashboard.jsx
@@ -588,7 +588,7 @@ console.log("บริษัทที่เลือก:", selectedCompany);
   if (!user) return null;
 
   return (
-  <div className="p-4 sm:p-6 bg-gray-50 min-h-screen">
+  <div className="p-4 sm:p-6 min-h-screen">
   <h1 className="text-xl sm:text-2xl font-bold mb-4 text-gray-800">
     ตารางบันทึกการลงเวลา
   </h1>
